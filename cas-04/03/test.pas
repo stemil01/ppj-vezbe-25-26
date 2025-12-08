6876f8884ee581a8158b9204667bbf01a628c77e
stemil01 <s.milenkovic@protonmail.com>
2025-12-09 00:13:53 +0100

    Add 'cas-04'.

---
+++ cas-04/03/test.pas
@@ -0,0 +1,21 @@
+(*ovo je ))) neki komentar*) Izmedju (***** *)
+(**** ****)
+(************)
+
+(*
+viselinijski komentar
+
+*)
+
+{
+viselinijski komentar
+}
+
+begin
+{jednolinijski komentar }
+Pravilno
+{viselinijski 
+komentar}
+Treci
+(* *)
+end.
---
+++ cas-04/06/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define sum(x, y) \
+    (x) + \
+    (  y)
+
+/* ovo je neki komentar */
+int main(int argc, char** argv) {
+
+    int x = 10; /* ovo je neki komentar */
+    double y = 43.394e-3; /* ovo je neki komentar */
+
+    /******************* 
+     * ovo je 
+     * neki 
+     * komentar 
+     *******************/
+    double z = x + y; // komentar
+
+    if (x > y)
+        z = 10;
+    else
+        z = -10.5;
+
+    /* ovo je neki 
+       komentar 
+     */
+    const char *str = "ovo je \" string";
+    printf("%lf\n", z);
+
+    /* ovo je neki komentar */
+    exit(EXIT_SUCCESS);
+}
